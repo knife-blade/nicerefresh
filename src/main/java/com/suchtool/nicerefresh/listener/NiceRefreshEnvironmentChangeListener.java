629da6cd80735a3b20555f57437060003f3ce657
knifeout <2716552863@qq.com>
2025-06-09 18:40:58 +0800

    1.0.7版本发布（支持更新日志打印）

--- src/main/java/com/suchtool/nicerefresh/listener/NiceRefreshEnvironmentChangeListener.java
+++ src/main/java/com/suchtool/nicerefresh/listener/NiceRefreshEnvironmentChangeListener.java
@@ -2,8 +2,11 @@ package com.suchtool.nicerefresh.listener;
 
 import com.suchtool.nicerefresh.context.NiceRefreshBeanField;
 import com.suchtool.nicerefresh.context.NiceRefreshBeanFieldHolder;
+import com.suchtool.nicerefresh.property.NiceRefreshProperty;
 import com.suchtool.nicetool.util.spring.AopUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -13,9 +16,13 @@ import org.springframework.util.CollectionUtils;
 import java.lang.reflect.Field;
 import java.util.List;
 
+@Slf4j
 public class NiceRefreshEnvironmentChangeListener implements
         ApplicationContextAware, ApplicationListener<EnvironmentChangeEvent> {
     private ApplicationContext applicationContext;
+
+    @Autowired(required = false)
+    private NiceRefreshProperty niceRefreshProperty;
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
@@ -47,6 +54,16 @@ public class NiceRefreshEnvironmentChangeListener implements
                             .getProperty(key, field.getType());
                     field.set(targetBean, value);
                     field.setAccessible(false);
+
+                    if (niceRefreshProperty.getEnableDebug() != null
+                        && niceRefreshProperty.getEnableDebug()) {
+                        log.info("Nicerefresh update property successfully。new value: {}, key: {}, beanName：{}, field: {}",
+                                value == null ? "null" : value.toString(),
+                                key,
+                                bean.getClass().getName(),
+                                field.getName()
+                        );
+                    }
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
